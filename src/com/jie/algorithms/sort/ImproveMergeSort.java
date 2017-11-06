d595ee37d124a3e23eeb08c18cabe1c0f45dc476
ThansksJava <luckyjiefeng@163.com>
2017-11-06 11:04:03 +0800

    不明白啥叫递归中交换参数避免数组复制？？

---
+++ src/com/jie/algorithms/sort/ImproveMergeSort.java
@@ -0,0 +1,78 @@
+package com.jie.algorithms.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by IntelliJ IDEA
+ *
+ * @author fengjie
+ * @Description
+ * @Date Created in 2017/11/3
+ * @Time 14:57
+ */
+public class ImproveMergeSort
+{
+    private static void merge(int[] index, int lo, int mid, int hi) {
+        //2、改进二
+        if(index[mid] <= index[mid+1]) {
+            return;
+        }
+        //3、改进三 交换参数避免数组复制???
+
+/*
+        int len = index.length;
+        // copy to aux[]
+        int[] aux = new int[len];
+        for (int k = lo;k <= hi; k++) {
+            aux[k] = index[k];
+        }
+        // merge back to a[]
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid) {
+                index[k] = aux[j++];
+            } else if (j > hi) {
+                index[k] = aux[i++];
+            } else if (aux[j] <= aux[i]) {
+                index[k] = aux[j++];
+            } else {
+                index[k] = aux[i++];
+            }
+        }
+*/
+    }
+    public static void insert(int[] a,int p,int r)
+    {
+        for(int i = p;i <= r;i++){
+            int key = a[i];
+            int j = i-1;
+            while(j >= p&& a[j] > key)
+            {
+                a[j+1] = a[j--];
+            }
+            a[j+1] = key;
+        }
+    }
+    public static void mergeSort(int [] a,int p,int r){
+        //1、改进一
+        int insertSortValue = 5;
+        if((r-p+1) <= insertSortValue){
+            insert(a,p,r);
+        }
+        if(p < r){
+            int q = (p+r)/2;
+            mergeSort(a,p,q);
+            mergeSort(a,q+1,r);
+            merge(a,p,q,r);
+        }
+    }
+    public static void main(String[] args) {
+        int [] a = new int[]{5,7,8,1,2,7,45,67,3,
+                25,134,4,2,3,45,1,6,34,234,234,24,
+                221,2,2,5,25,7,28,4,45,23,6,427,52,
+                6,52,7,236,23,4,3265,4,3,546,123,34,87,45,636,3,23421,4325};
+        System.out.println("排序前："+Arrays.toString(a));
+        mergeSort(a,5,20);
+        System.out.println("排序后："+Arrays.toString(a));
+    }
+}
