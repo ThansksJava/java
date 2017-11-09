1eb7234c0df98d1a476acf3dcf8ba49b601952dd
ThansksJava <luckyjiefeng@163.com>
2017-11-09 20:57:40 +0800

    测试测试

--- src/com/jie/algorithms/Test2.java
+++ src/com/jie/algorithms/Test2.java
@@ -1,4 +1,10 @@
 package com.jie.algorithms;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Test2 {
+    public static void main(String[] args) {
+        Arrays.toString(new int[]{1,2,3,4});
+    }
 }
