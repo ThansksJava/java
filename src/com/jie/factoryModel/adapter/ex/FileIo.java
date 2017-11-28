f7ef2c45de0a889f1be5f5f25771d230c460cd4e
ThansksJava <luckyjiefeng@163.com>
2017-11-28 10:10:03 +0800

    适配器练习题

---
+++ src/com/jie/factoryModel/adapter/ex/FileIo.java
@@ -0,0 +1,16 @@
+package com.jie.factoryModel.adapter.ex;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA
+ *
+ * @author fengjie
+ * @Description
+ * @Date Created in 2017/11/27
+ * @Time 9:19
+ */
+public interface FileIo {
+    void saveToFile() throws IOException;
+}
